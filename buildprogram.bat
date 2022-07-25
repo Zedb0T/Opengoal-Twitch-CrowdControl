04ec957171c05fd50579f36a7d7d38f2716b0ae2
Zedb0T <89345505+Zedb0T@users.noreply.github.com>
2022-07-25 11:09:55 -0400

    Update buildprogram.bat to prompt for dependecies

--- buildprogram.bat
+++ buildprogram.bat
@@ -1,3 +1,19 @@
+:PROMPT
+SET /P AREYOUSURE=Install pyinstaller (Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO dotenv
+pip install -U pyinstaller
+:dotenv
+SET /P AREYOUSURE=Install dotenv (Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO ahk
+pip install python-dotenv
+:ahk
+SET /P AREYOUSURE=Install ahk (Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO asyncio
+pip install ahk
+:asyncio
+SET /P AREYOUSURE=Install asyncio (Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO asyncio
+pip install asyncio
 set mypath=%~dp0
 pyinstaller --onefile resources\twitchcommands.py --add-data "C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python310\Lib\site-packages\ahk";ahk --icon resources\appicon.ico 
 move "%mypath%dist\twitchcommands.exe" "%mypath%/"
