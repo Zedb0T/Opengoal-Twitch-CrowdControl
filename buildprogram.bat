17175f5b3613931f2a9138b16daa6b73bfc494ad
Zedb0T <89345505+Zedb0T@users.noreply.github.com>
2022-07-25 10:32:01 -0400

    Cleanup Code

--- buildprogram.bat
+++ buildprogram.bat
@@ -1,4 +1,8 @@
 set mypath=%~dp0
-pyinstaller --onefile twitchcommands.py --add-data "C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python310\Lib\site-packages\ahk";ahk --icon appicon.ico 
+pyinstaller --onefile resources\twitchcommands.py --add-data "C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python310\Lib\site-packages\ahk";ahk --icon resources\appicon.ico 
 move "%mypath%dist\twitchcommands.exe" "%mypath%/"
+move "%mypath%\twitchcommands.spec" "%mypath%/resources"
 RENAME "%mypath%twitchcommands.exe" "RENAMEANDPUTNEAROPENGOAL.exe"
+@RD /S /Q "%mypath%/build"
+@RD /S /Q "%mypath%/dist"
+DEL /S /Q "%mypath%/twitchcommands.exe"
